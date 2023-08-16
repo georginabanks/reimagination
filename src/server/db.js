e148c9dcaa6420f14b129c2d563ece8e559e9f3f
Georgina Banks <georginabanks01@gmail.com>
2023-08-16 14:35:55 +0100

    process.env

--- src/server/db.js
+++ src/server/db.js
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize";
+import {DataTypes, Sequelize} from "sequelize";
 import Knex from "knex";
 
 const host = process.env.host;
@@ -16,7 +16,7 @@ export const dbConnection = {
 	user: username,
 	password: password,
 	database: database
-}
+};
 
 export const knex = Knex({
 	client: 'mysql2',
@@ -46,4 +46,47 @@ sequelize.authenticate().then(() => {
 	console.log('Connection has been established successfully.');
 }).catch((error) => {
 	console.error('Unable to connect to the database: ', error);
+});
+
+
+// Create Tables
+
+export const ClientLogo = sequelize.define("clientLogo", {
+	client_name: { type: DataTypes.STRING },
+	img_src: { type: DataTypes.STRING }
+}, { timestamps: false, tableName: "client_logos" });
+
+export const Page = sequelize.define("page", {
+	page_id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	page_name: { type: DataTypes.STRING },
+	content: { type: DataTypes.TEXT("medium") },
+	featured_image: { type: DataTypes.STRING }
+}, { timestamps: false, tableName: "pages" });
+
+export const Testimonial = sequelize.define("testimonial", {
+	testimonial_id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	name: { type: DataTypes.STRING },
+	bio: { type: DataTypes.STRING },
+	testimonial: { type: DataTypes.TEXT("medium") }
+}, { timestamps: false, tableName: "testimonials" });
+
+
+// Foreign Keys
+
+Testimonial.belongsTo(Page);
+Page.hasMany(Testimonial);
+
+
+sequelize.sync().then(() => {
+   console.log('Tables created successfully!');
+}).catch((error) => {
+   console.error('Unable to create table : ', error);
 });
